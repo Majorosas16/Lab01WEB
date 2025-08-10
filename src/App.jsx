b3fccb7036dc8546bf541f2d5fc188b9e34702d0
Majorosas16 <mariajose.rosascuellar@gmail.com>
2025-08-10 12:31:34 -0500

    header&FirstPart

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Button from './components/button/button'
+import Header from './components/header/header'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+return (
+  <main>
+    <Header />
+    <div className="hero-container"> 
+      <section className='Sec1'>
+        <h1>Escucha sin límites. Prueba 1 mes de Premium Individual por $ 0.</h1>
+        <p className='later'>Después, solo cuesta $ 18.500 por mes. Cancela cuando quieras.</p>
+        
+        <div className="button-group">
+          <Button title="Empezar" titleColor="var(--spotify-black)" background="#FCC6CE" hoverBg="#Ffff" hoverColor='#000' />
+          <Button title="Ver todos los planes" titleColor="var(--spotify-white)" background="transparent" border="1px solid var(--spotify-white)" />
+        </div>
+
+        <p className='dis'>
+          $ 0 por 1 mes. Después, cuesta $ 18.500 al mes. La oferta solo está disponible si aún no probaste Premium. Se aplican Términos.
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      </section>
+    </div>
+  </main>
+);
 }
 
 export default App
